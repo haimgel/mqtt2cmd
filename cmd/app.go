227584dc00b2d2e3b8169d13166e0343968db77b
Haim Gelfenbeyn <haim@g8n.me>
2022-10-02 16:11:24 -0400

    Instrument MQTT library for logging

--- cmd/app.go
+++ cmd/app.go
@@ -19,6 +19,7 @@ func Execute(version string, exit func(int), args []string) {
 	defer logger.Sync()
 	logger.Infow("Application started", "config_file", viper.ConfigFileUsed())
 
+	logging.InstrumentMqtt(logger)
 	client, err := mqtt.Init(appConfig.AppId, &appConfig.Mqtt, appConfig.Switches, logger)
 	if err != nil {
 		logger.Panic(err)
--- internal/logging/logging.go
+++ internal/logging/logging.go
@@ -1,6 +1,7 @@
 package logging
 
 import (
+	MQTT "github.com/eclipse/paho.mqtt.golang"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/natefinch/lumberjack.v2"
@@ -33,4 +34,39 @@ func CreateLogger(logFile string) *zap.SugaredLogger {
 		zap.WithCaller(false),
 	)
 	return logger.Sugar().With(zap.Int("pid", os.Getpid()))
+}
+
+type MqttLogger struct {
+	level zapcore.Level
+	inner *zap.SugaredLogger
+}
+
+func (logger MqttLogger) Println(v ...interface{}) {
+	switch logger.level {
+	case zapcore.DebugLevel:
+		logger.inner.Debugln(v)
+	case zapcore.WarnLevel:
+		logger.inner.Warnln(v)
+	default:
+		logger.inner.Errorln(v)
+	}
+}
+
+func (logger MqttLogger) Printf(format string, v ...interface{}) {
+	switch logger.level {
+	case zapcore.DebugLevel:
+		logger.inner.Debugf(format, v)
+	case zapcore.WarnLevel:
+		logger.inner.Warnf(format, v)
+	default:
+		logger.inner.Errorf(format, v)
+	}
+}
+
+func InstrumentMqtt(logger *zap.SugaredLogger) {
+	MQTT.CRITICAL = MqttLogger{inner: logger.With(zap.String("component", "mqtt")), level: zapcore.ErrorLevel}
+	MQTT.ERROR = MqttLogger{inner: logger.With(zap.String("component", "mqtt")), level: zapcore.ErrorLevel}
+	MQTT.WARN = MqttLogger{inner: logger.With(zap.String("component", "mqtt")), level: zapcore.WarnLevel}
+	// Debug logging is disabled, it's too chatty!
+	// MQTT.DEBUG = MqttLogger{inner: logger.With(zap.String("component", "mqtt")), level: zapcore.DebugLevel}
 }
